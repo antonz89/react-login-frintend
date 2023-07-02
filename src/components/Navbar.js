63d63a695fd4f6e08f1b9100a203ad0b28e6ad9a
antonz89 <anton.zakoverya@codeimmersives.com>
2023-07-01 21:02:52 -0400

    uploading frontend

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
+
+
+const Navbar = () => {
+  const { logout } = useLogout()
+  const { user } = useAuthContext()
+
+  const handleClick = () => {
+    logout()
+  }
+
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>Digital Closet</h1>
+        </Link>
+        <Link className="target-nav" to="/target">
+          <h1>Target Search</h1>
+        </Link>
+        <nav>
+          {user && (
+            <div>
+              <span className='nav-span'>{user.email}</span>
+              <button onClick={handleClick} >Log out</button>
+            </div>
+          )}
+          {!user &&(
+            <div>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </div>
+          )}  
+            
+
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+
+
+
+const Home = () => {
+
+
+
+    return (
+      <div className="home">
+       Logged In 
+      </div>
+    )
+  }
+  
+  export default Home
